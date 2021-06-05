eae3f91b1115ad196908a12253cfd2613180aec3
xjezzuuuu <xjezzuuuu@gmail.com>
2021-06-05 14:20:08 -0400

    Add webserver and restserver

--- 07-webserver/app.js
+++ 07-webserver/app.js
@@ -12,31 +12,32 @@ hbs.registerPartials(__dirname + '/views/partials');
 app.use(express.static('public'));
 
 // Routes
-app.get('/', function (req, res) {
-	res.render('home', {
-		nombre: 'Jesus Padilla',
-		title: 'Curso de NodeJS',
-	});
-});
+// app.get('/', function (req, res) {
+// 	res.render('home', {
+// 		nombre: 'Jesus Padilla',
+// 		title: 'Curso de NodeJS',
+// 	});
+// });
 
-app.get('/generic', function (req, res) {
-	res.render('generic', {
-		nombre: 'Jesus Padilla',
-		title: 'Curso de NodeJS',
-	});
-});
+// app.get('/generic', function (req, res) {
+// 	res.render('generic', {
+// 		nombre: 'Jesus Padilla',
+// 		title: 'Curso de NodeJS',
+// 	});
+// });
 
-app.get('/elements', function (req, res) {
-	res.render('elements', {
-		nombre: 'Jesus Padilla',
-		title: 'Curso de NodeJS',
-	});
-});
+// app.get('/elements', function (req, res) {
+// 	res.render('elements', {
+// 		nombre: 'Jesus Padilla',
+// 		title: 'Curso de NodeJS',
+// 	});
+// });
+
 app.get('*', function (req, res) {
 	res.status(404);
 	res.sendFile(__dirname + '/public/404.html');
 });
 
 app.listen(port, () => {
-	console.log('Server listen in the port:', port);
+	console.log(`Server listen at http://localhost:${port}`);
 });
